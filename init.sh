884b16a4f8150baba0afe7e9ccc848756d20747a
shogiru <rkvruru@gmail.com>
2019-09-23 15:35:27 +0200

    version 0.19

--- init.sh
+++ init.sh
@@ -1,6 +1,10 @@
 # sudo rm /etc/nginx/sites-enabled/default
+# sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test2.conf
+
 sudo cp /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test2.conf
+cat /etc/nginx/sites-enabled/test2.conf
 sudo /etc/init.d/nginx restart
+
 # sudo cp /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 # sudo /etc/init.d/gunicorn restart
 # sudo /etc/init.d/mysql start
