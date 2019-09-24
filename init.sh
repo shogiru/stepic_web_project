0408666db5e2acc0371fb5a8773a921a6a9f1dab
shogiru <rkvruru@gmail.com>
2019-09-24 22:58:33 +0200

    version 2.3

--- init.sh
+++ init.sh
@@ -1,7 +1,6 @@
 sudo rm /etc/nginx/sites-enabled/default
 sudo cp /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test2.conf
 sudo /etc/init.d/nginx
-
-# sudo cp /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-# sudo /etc/init.d/gunicorn restart
+sudo cp /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test.conf
+sudo /etc/init.d/gunicorn restart
 # sudo /etc/init.d/mysql start
