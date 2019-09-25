dd89f1f759766c42f60f833df41d4ca36ec58fc7
shogiru <rkvruru@gmail.com>
2019-09-25 23:12:16 +0200

    version 2.4

--- init.sh
+++ init.sh
@@ -1,6 +1,13 @@
+#!/bin/bash
+
 sudo rm /etc/nginx/sites-enabled/default
+echo "*** nginx default removed ***"
 sudo cp /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test2.conf
 sudo /etc/init.d/nginx
+echo "*** nginx started ***"
+
 sudo cp /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test.conf
 sudo /etc/init.d/gunicorn restart
+echo "*** gunicorn restarted ***"
+
 # sudo /etc/init.d/mysql start
