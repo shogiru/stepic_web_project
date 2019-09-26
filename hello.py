98d61a88011c4d752d060ab339aa55d3fdc54b3e
shogiru <rkvruru@gmail.com>
2019-09-26 20:09:23 +0200

    version 2.9

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
 def app(env, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain; charset=utf-8')])
-    body = "\r\n".join(env['QUERY_STRING'].split('&'))
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    body = "\r\n".join(env['QUERY_STRING'].split('&')).encode('utf-8')
     return body
