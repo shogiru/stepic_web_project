de60a3748a1d1a0c599dc0b74b61781bdef1536c
shogiru <rkvruru@gmail.com>
2019-09-26 20:04:56 +0200

    version 2.8

--- init.sh
+++ init.sh
@@ -1,13 +1,10 @@
-#!/bin/bash
+git clone https://github.com/shogiru/stepic_web_project.git /home/box/web
 
 sudo rm /etc/nginx/sites-enabled/default
-echo "*** nginx default removed ***"
-sudo cp /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test2.conf
-sudo /etc/init.d/nginx
-echo "*** nginx started ***"
+sudo cp /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
 
-sudo cp /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test.conf
-sudo /etc/init.d/gunicorn restart
-echo "*** gunicorn restarted ***"
+# sudo cp /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test.conf
+# sudo /etc/init.d/gunicorn restart
 
 # sudo /etc/init.d/mysql start
