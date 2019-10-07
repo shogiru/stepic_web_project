a746a65345bf6674c2e3da96532bb0b89493623c
shogiru <rkvruru@gmail.com>
2019-10-07 12:57:36 +0200

    version 3.5

--- init.sh
+++ init.sh
@@ -6,8 +6,8 @@ sudo /etc/init.d/nginx restart
 
 # gunicorn --bind='0.0.0.0:8080' hello:app
 
-sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:wsgi_application
-sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application
+# sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:app
+# sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application
 
 # sudo cp /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test.conf
 # sudo /etc/init.d/gunicorn restart
