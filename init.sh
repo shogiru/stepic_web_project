bcbad504e75eacf87dac49bb1387cdbbcdc9ab68
shogiru <rkvruru@gmail.com>
2019-10-07 07:11:32 +0200

    version 3 - done!

--- init.sh
+++ init.sh
@@ -4,7 +4,6 @@ sudo rm /etc/nginx/sites-enabled/default
 sudo cp /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
-cd web
 gunicorn --bind='0.0.0.0:8080' hello:app
 
 # sudo cp /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test.conf
