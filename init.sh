1269111745eb76b98ccb9d042e7dae79c0f5b9ea
shogiru <rkvruru@gmail.com>
2019-10-07 06:59:34 +0200

    version 3.0 - done!

--- init.sh
+++ init.sh
@@ -1,10 +1,16 @@
+#!/bin/bash
+
 git clone https://github.com/shogiru/stepic_web_project.git /home/box/web
 
 sudo rm /etc/nginx/sites-enabled/default
 sudo cp /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
+cd web
+gunicorn --bind='0.0.0.0:8080' hello:app
+
 # sudo cp /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test.conf
 # sudo /etc/init.d/gunicorn restart
 
 # sudo /etc/init.d/mysql start
+
